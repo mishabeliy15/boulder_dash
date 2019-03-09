fa4e1e46d9254e16a261be49fc19999214247bd2
mishabeliy15 <misha.beliy15@gmail.com>
2019-03-09 20:17:14 +0200

    Add project files.

---
+++ Boulder Dash/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Boulder_Dash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
